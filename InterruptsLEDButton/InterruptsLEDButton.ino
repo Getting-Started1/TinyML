93a82f52a0952cc2a94eb1b62c938aaf7c5f6785
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2024-08-09 11:00:35 +0300

    progress at page 112

--- InterruptsLEDButton/InterruptsLEDButton.ino
+++ InterruptsLEDButton/InterruptsLEDButton.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "mbed.h"
 
+#if defined(ARDUINO_ARDUINO_NANO33BLE)
+static const PinName gpio_pin_out = p23;
+static const PinName gpio_pin_in  = p30;
+#endif
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO)
+static const PinName gpio_pin_in = p28;
+static const PinName gpio_pin_out = p22;
+#endif
+
+static mbed::InterruptIn button(gpio_pin_in);
+static mbed::DigitalOut led(gpio_pin_out);
+
+void rise_ISR() {
+  led = 0;
+}
+
+void fall_ISR() {
+  led = 1;
+}
+
+void setup() {
+  button.mode(PullUp);
+  button.rise(&rise_ISR);
+  button.fall(&fall_ISR);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(4000);
 }
